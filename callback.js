f363f410e53c39b012ddecf1cb7e5642d753d350
Mouhannad <54218107+StrglrX@users.noreply.github.com>
2022-09-09 15:46:42 +0300

    Create callback.js

--- callback.js
+++ callback.js
@@ -1,19 +1,10 @@
-function(sinkDetails, sinks, interestingSinks) {
-   
-        window.listenerAdd = 1;
-        window.onload = () => {
-
-            setTimeout(() => {
-                window.opener.postMessage({"from":"strglrxSide"} ,"*");
-
-            }, 2000)
+    function (sinkDetails, sinks, interestingSinks) {
+        if (!((sinkDetails.value.startsWith("http") && !sinkDetails.value.startsWith("http://d0mxss")) || ((sinkDetails.value.startsWith("https") && !sinkDetails.value.startsWith("https://d0mxss"))) || (sinkDetails.sink == 'jQuery') || (sinkDetails.sink == 'jQuery.init') || (sinkDetails.sink == 'element.setAttribute.href' && sinkDetails.value.startsWith("/")) || ((sinkDetails.sink == 'location.replace' || sinkDetails.sink == 'location.href' || sinkDetails.sink == 'element.setAttribute.src' || sinkDetails.sink == 'location') && !(sinkDetails.value.startsWith("https://d0mxss") || sinkDetails.value.startsWith("http://d0mxss") || sinkDetails.value.startsWith("d0mxss")))) && sinkDetails.value.includes("d0mxss\"\"")) {
+            window.opener.postMessage({
+                "from": "strglrxSide",
+                "url": location.href
+            }, "*");
+            return true; //return true to log sink
         }
-  
-
-    if ((!sinkDetails.value.startsWith("http") || sinkDetails.value.startsWith("http://d0mxss") || sinkDetails.value.startsWith("https://d0mxss")) && sinkDetails.value.includes(sinkDetails.canary) && (sinkDetails.sink != 'jQuery' && sinkDetails.sink != 'jQuery.init') && !(sinkDetails.sink == 'element.setAttribute.href' && (!sinkDetails.value.startsWith("/d0mxss") && !sinkDetails.value.startsWith("d0mxss")))) {
-        window.listenerAdd = 1;
-        window.opener.postMessage({"from":"strglrxSide","url":location.href}, "*");
 
     }
-    return true; //return true to log sink
-}
