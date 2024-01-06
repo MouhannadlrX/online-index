f9b7380c7f4214fa4812b9decb26059904d1284c
Mouhannad <54218107+localhost-MouhannadlrX@users.noreply.github.com>
2024-01-06 18:05:55 +0300

    Delete automation-python directory

--- automation-python/remove-d0mxss.py
+++
@@ -1,9 +0,0 @@
-urls  = open("urls.txt","r")
-
-for url in urls:
-    list=url.split("d0mxsslocation.search")
-    s="".join(list)
-    list=url.split("d0mxsslocation.href")
-    s="".join(list)
-    print(s,end="")
-    
