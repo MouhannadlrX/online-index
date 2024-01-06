17e0868807f69d8a3b35be690555f9bf537aee42
Mouhannad <54218107+localhost-MouhannadlrX@users.noreply.github.com>
2024-01-06 18:07:12 +0300

    Delete msgCallback.js

--- msgCallback.js
+++
@@ -1,9 +0,0 @@
-function (msg) {
-    if (msg.confidence == 'Firm' || msg.confidence == 'Certain') {
-        window.opener.postMessage({
-            "from": "strglrxSide",
-            "url": location.href
-        }, "*");
-        return true; //return true to log sink
-    }
-}
